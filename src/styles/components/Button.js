e0b679317698cdeb89c2f9399aa6922d5927fb9f
mtkozlowski <kooziu@gmail.com>
2020-12-15 22:25:56 +0100

    improve Button and TextInput

--- src/styles/components/Button.js
+++ src/styles/components/Button.js
@@ -5,7 +5,7 @@ const Button = styled.input`
 
   height: 3rem;
   padding: 0.6rem 1rem 0.5rem;
-  margin: 1rem auto 1rem;
+  max-width: 120px;
   border-width: 0 0 1px 0;
   border-color: rgb(99 77 77 / 30%);
   box-shadow: 0px -1px 5px 1px rgb(99 77 77 / 10%);
--- src/styles/components/TextInput.js
+++ src/styles/components/TextInput.js
@@ -2,36 +2,36 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import styled from 'styled-components';
 
-const StyledLabel = styled.label`
+export const StyledLabel = styled.label`
   display: block;
-  padding: 0.6rem 0 0.5rem;
+  padding: 0.6rem 0 0;
 `;
 
 const StyledInput = styled.input`
   display: block;
   height: 3rem;
   padding: 0.6rem 1rem 0.5rem;
-  margin: 1rem auto 1rem;
-  width: 100%;
+  margin: 1rem 0 0;
   border-width: 0 0 1px 0;
   border-color: rgb(99 77 77 / 30%);
   box-shadow: 0px -1px 5px 1px rgb(99 77 77 / 10%);
+  width: 100%;
 `;
 
 const TextInput = ({ displayText, inputName, onChange }) => {
   const inputId = Math.random();
   return (
-    <>
-      <StyledLabel htmlFor={inputId}>{displayText}</StyledLabel>
+    <StyledLabel htmlFor={inputId}>
+      {displayText}
       <StyledInput name={inputName} id={inputId} onChange={onChange} />
-    </>
+    </StyledLabel>
   );
 };
 
 TextInput.propTypes = {
   displayText: PropTypes.string.isRequired,
   inputName: PropTypes.string.isRequired,
-  onChange: PropTypes.element.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default TextInput;
