6be0a8a42433f216a3b596a49f30e96a11a1fc1c
mtkozlowski <kooziu@gmail.com>
2020-12-16 21:52:51 +0100

    [feat] add custom highlighter

--- src/styles/components/ResultsList.js
+++ src/styles/components/ResultsList.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import styled from 'styled-components';
+import Highlighter from 'react-highlight-words';
 
 const StyledEntry = styled.li`
   border-bottom: 1px solid grey;
 `;
 
-const ResultLits = ({ results }) => {
+const ResultLits = ({ results, searchPhrase }) => {
   const elements = results.map((entry) => {
     return (
       <StyledEntry key={entry.pageId}>
         <h3>{entry.title}</h3>
-        <p dangerouslySetInnerHTML={{ __html: entry.snippet }} />
+        <p>
+          <Highlighter
+            highlightClassName="searchmatch"
+            searchWords={[searchPhrase]}
+            autoEscape
+            textToHighlight={entry.snippet}
+          />
+        </p>
       </StyledEntry>
     );
   });
@@ -25,10 +33,12 @@ const ResultLits = ({ results }) => {
 
 ResultLits.propTypes = {
   results: PropTypes.instanceOf(Array),
+  searchPhrase: PropTypes.string,
 };
 
 ResultLits.defaultProps = {
   results: [],
+  searchPhrase: '',
 };
 
 export default ResultLits;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -16,24 +16,6 @@ export function buildUrl(searchPhrase) {
   return url;
 }
 
-export async function getDataFromWiki(searchPhrase) {
-  const url = buildUrl(searchPhrase);
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    const datas = data.query.search.map(({ pageid, title, snippet }) => ({
-      pageId: pageid,
-      title,
-      snippet,
-    }));
-
-    return datas;
-  } catch (err) {
-    return new Error(err);
-  }
-}
-
 export function stripHtml(snippet) {
   const regex = /(?:<span class="searchmatch">)(\w+)(?:<\/span>)/gi;
   return snippet.replace(regex, '$1');
@@ -44,3 +26,21 @@ export const decodeHtml = (encodedText) => {
   shadowTextarea.innerHTML = encodedText;
   return shadowTextarea.value;
 };
+
+export async function getDataFromWiki(searchPhrase) {
+  const url = buildUrl(searchPhrase);
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const datas = data.query.search.map(({ pageid, title, snippet }) => ({
+      pageId: pageid,
+      title,
+      snippet: decodeHtml(stripHtml(snippet)),
+    }));
+
+    return datas;
+  } catch (err) {
+    return new Error(err);
+  }
+}
